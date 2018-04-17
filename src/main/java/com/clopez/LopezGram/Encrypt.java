80b6d257a67968b5b2d31e2b333178b3f42c2b69
Carlos Lopez <cluis.lopez@gmail.com>
2018-04-18 01:19:06 +0200

    Minor changes. Add static Encrypt class for future password management

---
+++ src/main/java/com/clopez/LopezGram/Encrypt.java
@@ -0,0 +1,43 @@
+package com.clopez.LopezGram;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class Encrypt {
+
+	public static String HashPasswd (String plainpasswd) throws NoSuchAlgorithmException {
+        byte[] salt = getSalt();
+         
+        String securePassword = get_SHA_256_SecurePassword(plainpasswd, salt);
+        return securePassword;
+	}
+	
+	private static String get_SHA_256_SecurePassword(String passwordToHash, byte[] salt) {
+        String generatedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(salt);
+            byte[] bytes = md.digest(passwordToHash.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++)
+            {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            generatedPassword = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        return generatedPassword;
+    }
+	
+    private static byte[] getSalt() throws NoSuchAlgorithmException
+    {
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+        byte[] salt = new byte[16];
+        sr.nextBytes(salt);
+        return salt;
+    }
+}
