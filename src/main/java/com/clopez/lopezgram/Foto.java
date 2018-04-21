5f65f7290334872d7be3a10d9487fd0746ada66b
Carlos Lopez <cluis.lopez@gmail.com>
2018-04-21 15:12:13 +0200

    Upload casi, casi

---
+++ src/main/java/com/clopez/lopezgram/Foto.java
@@ -0,0 +1,39 @@
+package com.clopez.lopezgram;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import com.google.appengine.tools.cloudstorage.GcsFileOptions;
+import com.google.appengine.tools.cloudstorage.GcsFilename;
+import com.google.appengine.tools.cloudstorage.GcsOutputChannel;
+import com.google.appengine.tools.cloudstorage.GcsService;
+import com.google.appengine.tools.cloudstorage.GcsServiceFactory;
+
+public class Foto {
+	private String name;
+	private String bucket;
+	private byte[] bytes;
+	
+	public Foto(String name, String bucket, byte[] bytes) {
+		this.name = name;
+		this.bucket = bucket;
+		this.bytes = bytes;
+	}
+
+	public boolean upload() {
+		GcsService gcsService = GcsServiceFactory.createGcsService();
+		GcsFileOptions options = new GcsFileOptions.Builder().mimeType("image/jpg").acl("public-read").build();
+		GcsFilename filename = new GcsFilename(bucket, name);
+		GcsOutputChannel writeChannel;
+		try {
+			writeChannel = gcsService.createOrReplace(filename, options);
+			writeChannel.write(ByteBuffer.wrap(bytes));
+			writeChannel.close();
+			return true;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+	}
+}
--- src/main/java/com/clopez/lopezgram/UploadEvent.java
+++ src/main/java/com/clopez/lopezgram/UploadEvent.java
@@ -3,28 +3,27 @@ package com.clopez.lopezgram;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
-import java.nio.ByteBuffer;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemIterator;
 import org.apache.commons.fileupload.FileItemStream;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
-
-import com.google.appengine.tools.cloudstorage.GcsFileOptions;
-import com.google.appengine.tools.cloudstorage.GcsFilename;
-import com.google.appengine.tools.cloudstorage.GcsOutputChannel;
-import com.google.appengine.tools.cloudstorage.GcsService;
-import com.google.appengine.tools.cloudstorage.GcsServiceFactory;
+import org.apache.commons.fileupload.util.Streams;
 
 import com.google.common.io.ByteStreams;
 
+@WebServlet("/UploadEvent")
 public class UploadEvent extends HttpServlet {
 	/**
 	 * 
@@ -32,59 +31,66 @@ public class UploadEvent extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-
-		// Get GCS service
-		GcsService gcsService = GcsServiceFactory.createGcsService();
-
-		// Set Option for that file
-		GcsFileOptions options = new GcsFileOptions.Builder().mimeType("image/jpg").acl("public-read").build();
-
-		// For multipart support
-		ServletFileUpload upload = new ServletFileUpload();
 		
+		// For multipart support
+		
+		ServletFileUpload upload = new ServletFileUpload();
+		// DefaultStorageProvider.setInstance(new MemoryStorageProvider());
+
+		String filename = "", bucket = "", ret = "";
+		byte[] bytes = new byte[0];
 		Map<String, String> mapa = new HashMap<>();
 
-		// Trying to create file
 		try {
 
 			FileItemIterator iterator = upload.getItemIterator(req);
-
+			//List<FileItem> items = upload.parseRequest(req);
+			//Iterator<FileItem> iterator = items.iterator();
+			
 			while (iterator.hasNext()) {
 				FileItemStream item = iterator.next();
+			    InputStream stream = item.openStream();
 				if (item.isFormField()) {
-					String name = item.getFieldName();
-				    String value = item.getName();
-				    mapa.put(name, value);
+					String key = item.getFieldName();
+					String value = Streams.asString(stream);
+					System.out.println(key + ","+ value);
+					mapa.put(key, value);
 				} else {
-					InputStream stream = item.openStream();
-					String name = item.getName();
-					String bucket = item.getFieldName();
-					GcsFilename filename = new GcsFilename(bucket, name);
-					GcsOutputChannel writeChannel = gcsService.createOrReplace(filename, options);
-					byte[] bytes = ByteStreams.toByteArray(stream);
-				}
-
-				try {
-					// Write data from photo
-					writeChannel.write(ByteBuffer.wrap(bytes));
-
-				} finally {
-
-					writeChannel.close();
+					filename = item.getName();
+					bucket = item.getFieldName();
+					bytes = ByteStreams.toByteArray(stream); // De momento solo se puede subir una única foto
 					stream.close();
-					res.setStatus(HttpServletResponse.SC_CREATED);
-					res.setContentType("text/plain");
-					PrintWriter out = res.getWriter();
-					out.write(bucket + "/" + name);
-					out.flush();
-					out.close();
 				}
 			}
-
-		} catch (FileUploadException e) {
-
-			e.printStackTrace();
+		} catch (FileUploadException e1) {
+			e1.printStackTrace();
+			ret = "FAILED";
 		}
+		
+		if (DataStore.validToken(mapa.get("mail"), mapa.get("token"))) { // Token is valid
+			Event e = new Event();
+			e.text = mapa.get("text");
+			e.creator = mapa.get("mail");
+			e.name = mapa.get("user");
+			Foto foto = new Foto(filename, bucket, bytes);
+			if (foto.upload()) {
+				e.picture.add(filename);
+			} else {
+				e.picture.add("NOPICT");
+			}
+			DataStore.SaveEvent(e);
+			ret = "UPLOADED";
+		} else {
+			ret = "UNAUTHORIZED";
+		}
+
+
+		res.setStatus(HttpServletResponse.SC_CREATED);
+		res.setContentType("text/plain");
+		PrintWriter out = res.getWriter();
+		out.write(ret);
+		out.flush();
+		out.close();
 
 	}
 }
